fcb68938fac360005a1919f76bc94d40722ada2a
minji_105 <ajhjmj9@naver.com>
2025-06-16 17:22:21 +0900

    메인 페이지 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
+import Card from "@/components/Card";
+
 export default function Home() {
-  return <></>;
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/pokemon")
+      .then((res) => res.json())
+      .then((data) => setPokemonData(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!pokemonData)
+    return (
+      <div className="my-[80px] flex justify-center text-5xl">Loading...</div>
+    );
+
+  return (
+    <div className="grid grid-cols-2 gap-[2px] bg-black md:grid-cols-4 lg:grid-cols-6">
+      {pokemonData.map((el) => (
+        <Card key={el.id} pokemon={el} />
+      ))}
+    </div>
+  );
 }
